1b30af4b934b145a7fbb5a181901d3ca9a4cd552
Mohammed Saqhib <125188740+Mohammed-Saqhib@users.noreply.github.com>
2025-05-06 22:13:55 +0530

    Delete my-single-page-website - (Working 4) directory

--- my-single-page-website - (Working 4)/js/service-worker.js
+++
@@ -1,3 +0,0 @@
-// This file is deprecated. Please use the service worker at the root level instead.
-// The file will be removed in future updates.
-console.warn('This service worker is deprecated. Using root level service-worker.js instead.');
